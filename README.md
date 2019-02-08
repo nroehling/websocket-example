9d7edf7e1a9e08b31feb54704927e743c1c7c684
nroehling <niklas.roehling@fh-bielefeld.de>
2019-02-08 02:41:23 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# WEBSOCKET EXAMPLE
+
+This example uses node.js and the ws library. Clientside is pure javascript with HTML 5.
+
+## INSTALL
+
+### Prerequisites
+
+- Nodejs
+- Npm
+
+### Install
+
+Clone the Git Repo.
+
+Install all npm dependencies (ws, requirejs)
+```
+npm install
+```
+
+## Usage
+In project directory and a Terminal
+```
+npm start
+```
+Let the Terminal openend so you can see Status messages from the server.
+
+Open the client.html both times in a browser of your choice. Open the Console in both browsers.
+Click the send test message button to send a test message to the server and the server will broadcoast to all connected clients in realtime!
---
+++ server.js
@@ -0,0 +1,26 @@
+const WebSocket = require('ws');
+
+const wss = new WebSocket.Server({ port: 8080 });
+
+wss.on('connection', ws => {
+    console.log("A Client established the Connection to the Websocket Server!");
+
+    ws.on('message', message => {
+        console.log(`Received message => ${message}`);
+        // Broadcast to everyone else.
+        wss.clients.forEach(function each(client) {
+            if (client.readyState === WebSocket.OPEN) {
+                client.send(message);
+            }
+        });
+    });
+    //ws.send('ho!');
+
+    ws.on('error', error => {
+        console.log("An error occured " + error);
+    });
+
+    ws.on('close', () => {
+        console.log("Client closed the connection!");
+    })
+});
